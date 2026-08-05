036967409907b7913bf1caff4ee52d7bdf106b3d
gboy02072008-byte <gboy02072008@gmail.com>
2026-08-05 17:58:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ def analyze_image_with_gemini(api_key, image):
     # รายชื่อโมเดลที่จะทดลองเรียกตามลำดับ
     models_to_try = [
         "gemini-1.5-flash",
-        "gemini-2.0-flash",
-        "gemini-1.5-pro"
+        "gemini-2.0-flash"
     ]
     
     prompt = (
